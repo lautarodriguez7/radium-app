8759ad140b7f25fd6024e90d8178aebafd54a258
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-09-25 19:03:15 -0300

    code added

---
+++ src/components/routes/header/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './header.css';
+
+class Header extends React.Component {
+  render() {
+     return (
+    <div className="mainContainer">
+        <div className="mainHeader">
+            Esto es el header
+        </div>
+    </div>
+    );
+  }
+ 
+}
+
+export default Header;
---
+++ src/components/routes/main-screen copy/main-screen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './main-screen.css';
+import header from '../header/header.js'
+class MainScreen extends React.Component {
+  render() {
+     return (
+      <div className="mainContainer">
+          <header />
+      </div>
+    );
+  }
+ 
+}
+
+export default MainScreen;
